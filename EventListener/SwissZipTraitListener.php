c4d0a12a38a8d426d5f9d714be9cd59e2dd99d96
Maurizio Monticelli <rugbymauri@gmail.com>
2021-09-03 11:29:22 +0200

    feat: use indices

---
+++ EventListener/SwissZipTraitListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace whatwedo\SwissZip\EventListener;
+
+use whatwedo\SwissZip\Entity\SwissZipTrait;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+
+class SwissZipTraitListener implements EventSubscriber
+{
+    public function getSubscribedEvents()
+    {
+        return [
+            'loadClassMetadata',
+        ];
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+    {
+        $cm = $eventArgs->getClassMetadata();
+        $class = $cm->getName();
+        $uses = class_uses($class);
+
+        if (in_array(SwissZipTrait::class, $uses)) {
+            $cm->table['indexes'][] = [
+                'columns' => [
+                    'postleitzahl',
+                ],
+            ];
+            $cm->table['indexes'][] = [
+                'columns' => [
+                    'plz_zz',
+                ],
+            ];
+            $cm->table['indexes'][] = [
+                'columns' => [
+                    'ortbez18',
+                ],
+            ];
+            $cm->table['indexes'][] = [
+                'columns' => [
+                    'ortbez27',
+                ],
+            ];
+        }
+    }
+}
